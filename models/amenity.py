466db358d97f870ec81a014ec55be604d10d9e0b
wanjikumutuku <moreenkinyua4@gmail.com>
2024-03-20 12:02:56 -0700

    number 8 and 10

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Amenity Module for HBNB project"""
+from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel):
+    """The Amenity class, contains information about amenities"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+
